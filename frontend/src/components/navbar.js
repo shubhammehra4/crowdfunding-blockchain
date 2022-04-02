e2f863a6eefd256b0f89b7c3cde4352f3fd29bdb
soham6125 <chaudhuri.ber@gmail.com>
2022-04-02 12:36:42 +0530

    connect wallet button

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/navbar.css'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [isConnected, setIsConnected] = useState(false);
+    
+    const onLogin = () => {
+        setIsConnected(true);
+    }
+
+    const onLogout = () => {
+        setIsConnected(false);
+    }
+
+    const detectProvider = () => {
+        let provider;
+        if (window.ethereum) {
+            provider = window.ethereum;
+        } else if (window.web3) {
+            provider = window.web3.currentProvider;
+        } else {
+            window.alert("No Ethereum browser detected!");
+        }
+        return provider;
+    };
+
+    const onLoginHandler = async () => {
+        const provider = detectProvider();
+        if (provider) {
+            if(provider !== window.ethereum) {
+                console.error(
+                    "Not window.ethereum provider."
+                );
+            }
+            await provider.request({
+                method: "eth_requestAccounts",
+            });
+            onLogin();
+        }
+    };
 
     return (
         <div className='navbar'>
@@ -15,7 +51,7 @@ function Navbar() {
                     <li><Link to="/" activeClass="active" style={{ fontSize: 16 }}> My Investments </Link></li>
                     <li><Link to="/" activeClass="active" style={{ fontSize: 16 }}> Pending Decisions </Link></li>
                 </ul>
-                <button onClick={connect} id="btn"> Connect Wallet </button>
+                <button onClick={onLoginHandler} id="btn"> Connect Wallet </button>
             </div>
         </div>
     )
